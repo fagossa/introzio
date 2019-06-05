6821e9f42ab0d8665a1dab503852e65ad34ecb7b
fagossa <fagossa@gmail.com>
2019-06-05 13:42:39 +0200

    replace Task.effect(...) by Task.apply(...)

--- src/main/scala/zio/service/LogService.scala
+++ src/main/scala/zio/service/LogService.scala
@@ -16,10 +16,10 @@ object LogService extends IOResources {
   def readFileContents(file: File): Task[FileContent] = {
     import scala.io.Source
     // We use brackets to close resources
-    Task.effect(Source.fromFile(file)(scala.io.Codec.UTF8)).bracket {
+    Task(Source.fromFile(file)(scala.io.Codec.UTF8)).bracket {
       close
     } { source =>
-      Task.effect(source.getLines().mkString("\n")).map {
+      Task(source.getLines().mkString("\n")).map {
         content => FileContent(file.getName, content)
       }
     }
@@ -30,8 +30,8 @@ object LogService extends IOResources {
     import com.lucidchart.open.xtract.XmlReader
     import scala.xml.XML
     for {
-      // handle event that can launch exceptions
-      xml <- Task.effect(XML.loadString(contents.contents))
+      // effect === apply := handle event that can launch exceptions
+      xml <- Task(XML.loadString(contents.contents))
       // handle event that DOES NOT launch exceptions
       maybeLog <- Task.effectTotal(XmlReader.of[Log].read(xml).toOption)
       response <- Task.effectTotal(maybeLog.map(LogWithName(contents.fileName, _)))
@@ -42,11 +42,11 @@ object LogService extends IOResources {
     import java.io._
     val file = new File(s"$dest/${log.fileName.replaceAll(".xml", "")}.csv")
     for {
-      _ <- Task.effect(new PrintWriter(file)).bracket {
+      _ <- Task(new PrintWriter(file)).bracket {
         close
       } { writer =>
         for {
-          _ <- Task.effect(writer.write("from, to, text\n"))
+          _ <- Task(writer.write("from, to, text\n"))
           // Same as Traverse[T]
           _ <- ZIO.foreach(log.log.messages) { message => Task.effect(writer.write(s"${Message.toLine(message)}\n")) }
         } yield ()
