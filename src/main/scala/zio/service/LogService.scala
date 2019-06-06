915dbe726c84362356eafd8787e1fed269926398
Fabian Gutierrez <fagossa@gmail.com>
2019-06-06 22:16:07 +0200

    some refactoring

--- src/main/scala/zio/service/LogService.scala
+++ src/main/scala/zio/service/LogService.scala
@@ -16,42 +16,59 @@ object LogService extends IOResources {
   def readFileContents(file: File): Task[FileContent] = {
     import scala.io.Source
     // We use brackets to close resources
-    Task(Source.fromFile(file)(scala.io.Codec.UTF8)).bracket {
-      close
-    } { source =>
-      Task(source.getLines().mkString("\n")).map {
-        content => FileContent(file.getName, content)
+    Task(Source.fromFile(file)(scala.io.Codec.UTF8)).bracket(close){ source =>
+      Task(source.getLines().mkString("\n"))
+        .map { content => FileContent(file.getName, content)
       }
     }
   }
 
-  // TODO: replace this with an either?
   def transformToMessage(contents: FileContent): Task[Option[LogWithName]] = {
     import com.lucidchart.open.xtract.XmlReader
     import scala.xml.XML
     for {
       // effect === apply := handle event that can launch exceptions
       xml <- Task(XML.loadString(contents.contents))
+      // handle an already ready event
+      maybeLog <- Task.succeed(XmlReader.of[Log].read(xml).toOption)
       // handle event that DOES NOT launch exceptions
-      maybeLog <- Task.effectTotal(XmlReader.of[Log].read(xml).toOption)
       response <- Task.effectTotal(maybeLog.map(LogWithName(contents.fileName, _)))
     } yield response
   }
 
-  def writeLogToFile(dest: String)(log: LogWithName): Task[Unit] = {
+  def writeLogToFile(dest: String)(log: LogWithName): Task[Receipt] = {
+    // val t: Task[Unit] = blocking.effectBlocking(())
     import java.io._
     val file = new File(s"$dest/${log.fileName.replaceAll(".xml", "")}.csv")
     for {
-      _ <- Task(new PrintWriter(file)).bracket {
-        close
-      } { writer =>
+      receipts <- Task(new PrintWriter(file)).bracket(close){ writer =>
         for {
-          _ <- Task(writer.write("from, to, text\n"))
+          _        <- Task(writer.write("from, to, text\n"))
           // Same as Traverse[T]
-          _ <- ZIO.foreach(log.log.messages) { message => Task.effect(writer.write(s"${Message.toLine(message)}\n")) }
-        } yield ()
+          // Max 10 threads: ZIO.foreachParN(10)(log.log.messages)
+          receipts <- ZIO.foreach(log.log.messages) { message =>
+                        // Transform each individual message into a Receipt
+                        Task.effect(writer.write(s"${Message.toLine(message)}\n"))
+                          .fold(t => Receipt.failure(log.fileName, t), _ => Receipt.success(log.fileName))
+                     }
+        } yield receipts
       }
-    } yield ()
+    } yield receipts.foldLeft(Receipt.empty){ _ |+| _ }
   }
 
 }
+
+case class Receipt(value: Map[String, Option[Throwable]]) { self =>
+  final def |+|(that: Receipt): Receipt =
+    Receipt(self.value ++ that.value)
+
+  final def succeeded: Int = value.values.count(_.isEmpty)
+  final def failures: List[Throwable] = value.values.collect { case Some(t) => t}.toList
+
+}
+object Receipt {
+  def empty: Receipt = Receipt(Map.empty)
+  def success(fileName: String): Receipt = Receipt(Map(fileName -> None))
+  def failure(fileName: String, t: Throwable): Receipt = Receipt(Map(fileName -> Some(t)))
+}
+
